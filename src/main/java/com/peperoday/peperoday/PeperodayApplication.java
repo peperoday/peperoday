178e2086d024a6cb880d9f65b947ae4b5c47437f
KoHanSol <abc35572003@naver.com>
2022-05-04 15:51:05 +0900

    프로젝트 초기 설정 셋팅

---
+++ src/main/java/com/peperoday/peperoday/PeperodayApplication.java
@@ -0,0 +1,13 @@
+package com.peperoday.peperoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeperodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeperodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peperoday/peperoday/PeperodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peperoday.peperoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeperodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
