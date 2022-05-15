15e4cb58d665a9903aaa7867ca9852c1917215d9
KoHanSol <abc35572003@naver.com>
2022-05-15 23:01:25 +0900

    [로그인 기능] - 로그인, 회원가입 페이지 구현

---
+++ src/main/java/com/peperoday/peperoday/member/domain/Member.java
@@ -0,0 +1,49 @@
+package com.peperoday.peperoday.member.domain;
+
+import com.peperoday.peperoday.member.domain.Role;
+import com.peperoday.peperoday.utiles.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Member extends BaseTimeEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "mber_srno")
+    private Long srNo;
+
+    @Column(name = "mber_id", nullable = false)
+    private String mberId;
+
+    @Column(name = "mber_pw", nullable = false)
+    private String mberPw;
+
+    @Column(name = "mber_nm", nullable = false)
+    private String mberNm;
+
+    @Column(name = "mber_email", nullable = false)
+    private String mberEmail;
+
+    //탈퇴여부: withdrawal
+    @ColumnDefault("N")
+    private boolean withdrawal;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public Member(String mberId, String mberPw, String mberEmail, String mberNm){
+        this.mberId = mberId;
+        this.mberPw = mberPw;
+        this.mberEmail = mberEmail;
+        this.mberNm = mberNm;
+    }
+}
+
---
+++ src/main/java/com/peperoday/peperoday/member/service/Impl/MemberServiceImpl.java
@@ -0,0 +1,11 @@
+package com.peperoday.peperoday.member.service.Impl;
+
+import com.peperoday.peperoday.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class MemberServiceImpl implements MemberService {
+}
