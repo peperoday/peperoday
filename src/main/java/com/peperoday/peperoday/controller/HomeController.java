819c8cdfdf0918af2cf4f4f4cd6203f65307331e
KoHanSol <abc35572003@naver.com>
2022-05-17 19:55:14 +0900

    [페이지 이동] 페이지 조회는 HomeController에서 처리하도록 수정

--- src/main/java/com/peperoday/peperoday/controller/HomeController.java
+++ src/main/java/com/peperoday/peperoday/controller/HomeController.java
@@ -6,10 +6,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-
+    //메인 페이지
     @GetMapping("/")
     public String home(){
+
         return "index";
     }
+    
+    //로그인 페이지
+    @GetMapping("/loginPage")
+    public String loginPage(){
 
+        return "/members/loginPage";
+    }
+    
+    //회원가입 페이지
+    @GetMapping("/joinPage")
+    public String joinPage(){
+
+        return "/members/joinPage";
+    }
+    
+    //타임라인게시판 페이지
+    @GetMapping("/communityHome")
+    public String communityPage(){
+
+        return "/community/communityTimeline";
+    }
+    
+    //커뮤니티게시판 페이지
+    @GetMapping("/communityBoard")
+    public String communityBoard(){
+
+        return "/community/communityBoard";
+    }
 }
