34c72de9d107d69d83bf55824fb17fbe60e3cc4f
khs <gustjd1209@naver.com>
2022-05-24 14:03:37 +0900

    TEST

--- src/main/java/com/peperoday/peperoday/repository/BoardRepository.java
+++ src/main/java/com/peperoday/peperoday/repository/BoardRepository.java
@@ -4,5 +4,5 @@ import com.peperoday.peperoday.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    // 한번더 TEST
+    // 한번더 TEST  sdf
 }
